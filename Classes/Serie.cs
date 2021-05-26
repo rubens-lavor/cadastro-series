21a08b46e6ca44129329b3b0c3768da2522cef36
rubens-lavor <haryson07@gmail.com>
2021-05-25 22:12:06 -0300

    aula 2 concluída

---
+++ Classes/Serie.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace ProjetoDIO.Series
+{
+  public class Serie : EntidadeBase
+  {
+
+    private Genero Genero { get; set; }
+    private string Titulo { get; set; }
+    private string Descricao { get; set; }
+    private int Ano { get; set; }
+
+
+    public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+  {
+    this.Id = id;
+    this.Genero = genero;
+    this.Titulo = titulo;
+    this.Descricao = descricao;
+    this.Ano = ano;
+  }
+
+
+    public override string ToString()
+    {
+      string retorno = " ";
+      retorno += "Genero: " + this.Genero + Environment.NewLine;
+      retorno += "Título: " + this.Titulo + Environment.NewLine;
+      retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+      retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+
+      return retorno;
+    }
+
+    public string retornarTitulo(){
+      return this.Titulo;
+    }
+
+    internal int retornarId(){
+      return this.Id;
+    }
+  }
+}
